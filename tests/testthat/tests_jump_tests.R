4f7dd6ec7eb774c63ad587b2dc4d3bbdb10d1eec
onnokleen <mail@onnokleen.de>
2019-05-29 14:56:19 +0200

    more jump tests

--- tests/testthat/tests_jump_tests.R
+++ tests/testthat/tests_jump_tests.R
@@ -8,3 +8,15 @@ expect_equal(
   formatC(JOjumptest(sample_5minprices_jumps[,1], power = 6)$ztest, digits = 10),
   "8.998787842"
 )
+
+expect_equal(
+  formatC(JOjumptest(sample_5minprices_jumps[,1], power = 4)$ztest, digits = 4),
+  "7.832"
+)
+
+expect_equal(
+  formatC(BNSjumptest(sample_tdata$PRICE, IVestimator= "minRV", 209, IQestimator = "medRQ", type= "linear", makeReturns = TRUE)$pvalue, digits = 0),
+  c(PRICE = "0")
+)
+
+
