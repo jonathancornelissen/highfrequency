c7892b5634b1c969780e3346f6686ae81a0f7399
onnokleen <mail@onnokleen.de>
2019-05-29 14:19:04 +0200

    second harModel test

--- R/har_model.R
+++ R/har_model.R
@@ -137,7 +137,6 @@ harInsanityFilter <- function(fittedValues, lower, upper, replacement) {
 #' @keywords forecasting
 #' 
 #' @examples
-#' \dontrun{
 #' ##### Example 1: HARRVCJ ##### 
 #' data("sample_5minprices_jumps")
 #' dat <- sample_5minprices_jumps[, 1]
@@ -214,7 +213,7 @@ harInsanityFilter <- function(fittedValues, lower, upper, replacement) {
 #' x
 #' #plot(x)
 #' predict(x)
-#' }
+#' 
 #' @export
 harModel <- function(data, periods = c(1,5,22), periodsJ = c(1,5,22), periodsQ = c(1),
                      leverage=NULL, RVest = c("rCov","rBPCov", "rQuar"), type = "HARRV", inputType = "RM",
@@ -527,7 +526,7 @@ plot.harModel <- function(x, which = c(1L:3L, 5L), caption = list("Residuals vs 
 
 #' @importFrom stats var
 #' @export
-predict.harModel <- function(object, newdata = NULL, warnings = TRUE) {
+predict.harModel <- function(object, newdata = NULL, warnings = TRUE, ...) {
   # If no new data is provided - just forecast on the last day of your estimation sample
   # If new data with colnames as in object$model$x is provided, i.e. right measures for that model, just use that data
   ##### These 4 lines are added to make adding new models (hopefully) easier
@@ -838,49 +837,6 @@ predict.harModel <- function(object, newdata = NULL, warnings = TRUE) {
       }
       return(as.numeric(as.matrix(cbind(1, x))  %*%  object$coefficients))
     }
-  }
-}
-
-#' @export
-predict.harModel <- function(object, newdata = NULL, ...) {
-  
-  # If no new data is provided - just forecast on the last day of your estimation sample
-  # If new data with colnames as in object$model$x is provided, i.e. right measures for that model, just use that data
-  if (is.null(newdata)) {
-    if (is.null(object$transform) == TRUE) {
-      return(as.numeric(c(1, xts::last(object$model$x))  %*%  x$coefficients))
-    }
-    
-    if (object$transform == "log") {
-      print("Due to log-transform, forecast of RV is derived under assumption of log-normality.")
-      return(as.numeric(exp(c(1, xts::last(object$model$x))  %*%  object$coefficients + 1/2 * var(object$residuals))))
-    }
-    
-    if (object$transform == "sqrt") {
-      print("Forecast for sqrt(RV) due to transform == \"sqrt\".")
-      return(as.numeric(c(1, xts::last(object$model$x))  %*%  object$coefficients))
-    }
-  }
-  
-  # Check whether newdata is in right format
-  if (sum(colnames(newdata) == colnames(object$model$x)) == length(colnames(object$model$x))) {
-    
-    if (is.null(object$transform) == TRUE) {
-      return(as.numeric(cbind(1, newdata)  %*%  object$coefficients))
-    }
-    
-    if (object$transform == "log") {
-      print("Due to log-transform, forecast of RV is derived under assumption of log-normality.")
-      return(as.numeric(exp(cbind(1, newdata)  %*%  object$coefficients + 1/2 * var(object$residuals))))
-    }
-    
-    if (object$transform == "sqrt") {
-      print("Forecast for sqrt(RV) due to transform == \"sqrt\".")
-      return(as.numeric(cbind(1, newdata)  %*%  object$coefficients))
-    }
-    
-  } else {
-    warning(paste0(c("newdata column names should be", colnames(object$model$x), "as harModel-type is", object$type, "."), collapse = " "))
   }
 }
 
--- tests/testthat/tests_models.R
+++ tests/testthat/tests_models.R
@@ -5,3 +5,10 @@ expect_equal(
   "7.6406"
 )
 
+expect_identical(
+  {blub <- harModel(makeReturns(sample_5minprices_jumps[, 1]), periods = c(1, 5, 10), periodsJ = c(1, 5, 10), 
+           RVest = c("rCov", "rBPCov"), type = "HARRVCJ", transform = "sqrt", inputType = "returns")
+  blub2 <- plot(blub)
+  blub2$get_xlim()},
+  c(1263916800, 1264780800)
+)
