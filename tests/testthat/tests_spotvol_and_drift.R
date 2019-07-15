28dfafab253f71bb3d2b0c7ddb5aa2de4cc4b7eb
onnokleen <mail@onnokleen.de>
2019-07-15 23:38:49 +0200

    new test!

--- tests/testthat/tests_spotvol_and_drift.R
+++ tests/testthat/tests_spotvol_and_drift.R
@@ -5,3 +5,11 @@ expect_identical(
   formatC(kerneldrift$mu[1:3], digits = 5)},
   c("     0", "-0.070876", "-0.1145")
 )
+
+
+expect_identical(
+  {dat <- sample_tdata_microseconds[, SYMBOL := NULL]
+  meandrift <- spotDrift(data = dat, k = 1, tz = "EST")
+  formatC(meandrift$mu[1:10], digits = 5)},
+  c("    NA", "    NA", "    NA", "    NA", "0.00040961", "0.000454", "0.00060476", "0.00051623", "0.00050264", "-2.5183e-05")
+)
