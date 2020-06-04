a773640ee87de1c6e4daa057494faa9abf8d774a
emil <emilsjoerup@live.dk>
2020-06-04 09:37:20 +0200

    Jump variation no longer decreased when due scaling the the volatility first.

--- R/simulations.R
+++ R/simulations.R
@@ -6,20 +6,23 @@ hfsim.do <- function(hfSimSpec){
   nObs <- hfSimSpec$nObs
   nSeries <- hfSimSpec$nSeries
   nDays <- hfSimSpec$nDays
-  
+  includeJumps <- hfSimSpec$jumpModel$includeJumps
   model <- hfSimSpec$model
   jumpModel <- hfSimSpec$jumpModel
   misc <- hfSimSpec$misc
   
   # Here, we must lower the volatility of the volatility process to account for the jump variation
   if (jumpModel$modelType != "none") {
+    jumpVolatility <- model$volatility * jumpModel$jumpVariation # We create a variable to contain the jump volatility which will be used in the RNG
+    
     model$volatility <- model$volatility * (1-jumpModel$jumpVariation)
+    
   }
   
   if(jumpModel$modelType == "pre announced"){
     jumpModel$modelType <- "PA" # We will use the abbreviation in the switch statement
   }
-  
+
   
   ## First we generate a drift path
   driftPath <- switch (model$driftModel,
@@ -34,11 +37,8 @@ hfsim.do <- function(hfSimSpec){
   ## We generate a jump
   jumpPath <- switch (jumpModel$modelType,
                        none = matrix(0,1,nSeries),
-                       PA = matrix(rnorm(nDays * nSeries) * sqrt(model$volatility * jumpModel$jumpVariation) , ncol = nSeries)
+                       PA = matrix(rnorm(nDays * nSeries) * sqrt(jumpVolatility) , ncol = nSeries)
   )
-  
-  lower <- jumpModel$jumpTime[1] * nObs
-  upper <- jumpModel$jumpTime[2] * nObs
   
   jumpIndices <- switch(jumpModel$modelType,
                          none = matrix(1, 1, nSeries),
@@ -50,17 +50,26 @@ hfsim.do <- function(hfSimSpec){
   jumpIndices <- matrix(apply(jumpIndices, 2, FUN = function(x) pmax.int(x, 1)), ncol = nSeries)
  
   
-  
-  out <- switch (model$modelType,
-                 BM = BrownianMotion(nDays, nObs, driftModel, volatilityModel),
-                 BMJ = BrownianMotionWithJumps(nDays, nObs, nSeries, driftModel, volatilityModel, jumpPath, jumpIndices)
+  sim <- switch (model$modelType,
+                 BM = BrownianMotion(nDays, nObs, nSeries, drift = model$drift, volatility = model$volatility),
+                 BMJ = BrownianMotionWithJumps(nDays, nObs, nSeries, drift = model$drift, volatility = model$volatility, jumpPath, jumpIndices)
   )
   
+  out <- vector(mode = "list", length = 4L)
+  names(out) <- c("prices", "drift", "volatility", "jumps")
   ## Convert to xts
   if(model$sampling == "equidistant"){
-    timestamps <- numeric(ifelse(is.matrix(out), nrow(out), length(out))) # If we have a multivariate output, we will use nrow.
-    timestamps <- rep(0:(nDays-1), each = nObs) * 86400 + seq(misc$trading.start, misc$trading.end, length.out = nObs)
-    out <- xts(out, as.POSIXct(timestamps, origin = misc$origin))  
+    timestamps <- numeric(ifelse(is.matrix(sim$prices), nrow(sim$prices), length(sim$prices))) # If we have a multivariate output, we will use nrow.
+    timestamps <- rep(0:(nDays-1), each = nObs) * 86400 + seq(misc$tradingStart, misc$tradingEnd, length.out = nObs)
+    out$prices <- xts(sim$prices, as.POSIXct(timestamps, origin = misc$origin))  
+    
+    # Here, we add timestamps to the jumps
+    if(includeJumps){
+      out$jumps <- xts(matrix(diag(nSeries), length(jumpIndices), ncol = nSeries, byrow = FALSE), as.POSIXct(timestamps[jumpIndices + rep(0:(nDays-1) * nObs, each = nSeries)], origin = misc$origin))
+    } else {
+      out$jumps <- NULL
+    }
+    
   }
   
   
@@ -72,16 +81,11 @@ hfsim.do <- function(hfSimSpec){
 #' @export
 createHFSimSpec <- function(model = list(modelType = "BM", driftModel = "constant", volatilityModel = "constant",  drift = 0,
                                    volatility = 0.05, alpha = NULL, beta = NULL, sampling = "equidistant"),
-                      jumpModel = list(modelType = "none", jumpVariation = 1 / 5, jumpTime = c(0.5, 17/32)),
+                      jumpModel = list(modelType = "none", jumpVariation = 1 / 5, jumpTime = c(0.5, 17/32), includeJumps = FALSE),
                       noiseModel = list(noiseType = "constant with interday change", signaltonoiseratio = 2),
                       nSeries = 1, nDays = 1, nObs = 23401, 
-                      discretize = FALSE, time.settings = list(trading.start = 34200, trading.end = 57600, origin = "1970-01-01" , sampling = "equidistant")){
+                      discretize = FALSE, time.settings = list(tradingStart = 34200, tradingEnd = 57600, origin = "1970-01-01" , sampling = "equidistant")){
 
-  
-  
-
-  
-  
   
   simSpec <- list(model = model, jumpModel = jumpModel, noiseModel = noiseModel, nSeries = nSeries, misc = misc,
                   nDays = nDays, nObs = nObs, discretize = discretize, time.settings = time.settings)
@@ -91,57 +95,81 @@ createHFSimSpec <- function(model = list(modelType = "BM", driftModel = "constan
 }
 
 #' @keywords internal
-BrownianMotion <- function(nDays, nObs, nSeries, driftModel, volatilityModel){
+BrownianMotion <- function(nDays, nObs, nSeries, drift, volatility){
   
-  out <- matrix(0, nrow = nDays * nObs, ncol = nSeries)
+  prices <- matrix(0, nrow = nDays * nObs, ncol = nSeries)
   # Vector denoting which observations to switch between days on.
   obs.vec <- seq(1, nObs * (nDays + 1), nObs)
 
+  # simulate the drift and volatility paths that will serve as the basis of our price simulation
   
-  driftPath <- simulateDrift(nObs, driftModel)
-  volatilityPath <- simulateVolatility(nObs, volatilityModel)
-  
-  out[1:nObs, ] <- apply(matrix(rnorm(nObs * nSeries, driftPath, volatilityPath) * sqrt(1/nObs), ncol = nSeries, byrow = TRUE), 2, cumsum)
+  returns <- matrix(rnorm(nObs * nSeries, drift, volatility) * sqrt(1/nObs), ncol = nSeries)
+  prices[1:nObs, ] <- colCumsum(returns)
   for (i in 2:nDays) {
-    out[obs.vec[i]:(obs.vec[(i+1)]-1), ] = colCumsum(matrix(c(out[obs.vec[i]-1,] + rnorm(1, driftPath, volatilityPath) * sqrt(1/nObs),
-                                                              rnorm(nObs-1, driftPath, volatilityPath) * sqrt(1/nObs), ), byrow = TRUE))
-      
+    returns <- matrix(rnorm((nObs) * nSeries, drift, volatility) * sqrt(1/nObs), ncol = nSeries)
+    returns[1,] <- prices[obs.vec[i]-1,] # set the initial values of the price
+    prices[obs.vec[i]:(obs.vec[(i+1)]-1),] <- colCumsum(returns)
   }
+  
+  out <- list(prices = prices)
+  
   
   return(out)
   
 }
 
-
 #' @keywords internal
-BrownianMotionWithJumps <- function(nDays, nObs, nSeries, driftModel, volatilityModel, jumpPath, jumpIndices){
+BrownianMotionWithJumps <- function(nDays, nObs, nSeries, drift, volatility, jumpPath, jumpIndices){
   
-  out <- matrix(0, nrow = nDays * nObs, ncol = nSeries)
+  
+  
+  prices <- matrix(0, nrow = nDays * nObs, ncol = nSeries)
   # Vector denoting which observations to switch between days on.
   obs.vec <- seq(1, nObs * (nDays + 1), nObs)
   
-  ### TODO: Fix the volatility and drift paths such that we can actually use these.
-  returns <- matrix(rnorm(nObs * nSeries, driftModel[1,], volatilityModel[1,]) * sqrt(1/nObs), ncol = nSeries)
+  
+  returns <- matrix(rnorm(nObs * nSeries, drift, volatility) * sqrt(1/nObs), ncol = nSeries)
   # Here, we insert the jumps
   for (j in 1:nSeries) {
     returns[jumpIndices[1,j], j] <- returns[jumpIndices[1,j], j] + jumpPath[1,j]
   }
   
-  out[1:nObs, ] <- apply(returns,2, cumsum)
+  prices[1:nObs, ] <- colCumsum(returns)
   for (i in 2:nDays) {
-    returns <- matrix(rnorm((nObs) * nSeries, driftModel[1,], volatilityModel[1,]) * sqrt(1/nObs), ncol = nSeries)
-    returns[1,] <- out[obs.vec[i]-1,] # set the initial values of the price
+    returns <- matrix(rnorm((nObs) * nSeries, drift, volatility) * sqrt(1/nObs), ncol = nSeries)
+    returns[1,] <- returns[1, ] + prices[obs.vec[i]-1,] # set the initial values of the price
     # Here, we insert the jumps
     for (j in 1:nSeries) {
       returns[jumpIndices[i,j], j] <- returns[jumpIndices[i,j], j] + jumpPath[i,j]
     }
     
-    
-    out[obs.vec[i]:(obs.vec[(i+1)]-1),] <- apply(returns, 2, cumsum)
+    prices[obs.vec[i]:(obs.vec[(i+1)]-1),] <- colCumsum(returns)
   }
   
+  out <- list(prices = prices)
   
   return(out)
 }
 
+
+simulateDrift <- function(model){
+  
+  if(model$driftModel == "constant"){
+    out <- model$drift
+  }
+  
+  
+  return(out)
+  
+  
+}
+
+simulateVolatility <- function(model){
+  
+  if(model$driftModel == "constant"){
+    out <- model$volatilityModel
+  }
+  
+  return(out)
+}
 
