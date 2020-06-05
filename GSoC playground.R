c89ebb0cf30e3a24fb2f32d93af72c7192a2b28a
emil <emilsjoerup@live.dk>
2020-06-05 21:46:56 +0200

    Further work on simulations with specs in playground

--- GSoC playground.R
+++ GSoC playground.R
@@ -1,9 +1,11 @@
 rm(list = ls())
+gc() # force garbage collection
 ####### GSoC 2020 playground #######
+library(data.table)
 library(highfrequency)
 library(quantmod)
 library(anytime)
-
+options(error = recover)
 ####### Alpha Vantage #######
 
 # Because this will be on github I will read my key from a file on my desktop.
@@ -16,61 +18,60 @@ if(FALSE)
   data <- getHFData(symbols = c("MMM", "GS", "SPY"), outputType = "DT")
 
 ####### Simulations #######
+volatilityModel <- list(modelType = "constant burst", variance = 0.2, burstModel = list(burstMultiplier = 3, burstInterval = c(16/32, 17/32)),
+                        includeDiurnality = FALSE, diurnalModel = list(C = 0.88929198, A = 0.75, B = 0.25, a = 10, b = 10))
+driftModel <- list(modelType = "constant", drift = 0)
+nSeries <- 1
+nDays <- 5
+nObs <- 23400 
+timeSettings  <- list(tradingStart = 34200, tradingEnd = 57600, origin = "1970-01-01" , sampling = "equidistant")
+discretize <- FALSE
 
-# defaults so it's easy to play with
-model = list(modelType = "BMJ", driftModel = "constant", volatilityModel = "constant",  drift = 0,
-             volatility = 1, alpha = NULL, beta = NULL, sampling = "equidistant")
-nSeries = 3
-nDays = 3
-nObs = 23400
-misc = list(trading.start = 34200, trading.end = 57600, origin = "1970-01-01")
-discretize = FALSE
+jumpModel  <- list(modelType = "none", jumpComponent = 1 / 5, jumpTime = c(0.25, 0.75)) #includeJumps should be automated in the creation of the spec
 
-jumpModel  <- list(modelType = "pre announced", jump.variation = 1 / 4, jump.time = c(0.25, 0.75))
-
-hfSimSpec <- createHFSimSpec(model = model, jumpModel = jumpModel, nDays = nDays, nSeries = nSeries, nObs = nObs)
+hfSimSpec <- createHFSimSpec(volatilityModel = volatilityModel, driftModel = driftModel, jumpModel = jumpModel, nDays = nDays, nSeries = nSeries, nObs = nObs)
 sim <- hfsim.do(hfSimSpec)
 
+gc()
 
-RV <- rCov(exp(sim), makeReturns = TRUE, align.by = 'mins', align.period = 1)
-BPV  <- rBPCov(exp(sim), makeReturns = TRUE, align.by = "mins", align.period = 1)
+rv <- rCov(exp(sim$prices), makeReturns = TRUE, align.by = "secs", align.period = 1)
+bpv  <- rBPCov(exp(sim$prices), makeReturns = TRUE, align.by = "secs", align.period = 1)
 
-ratio <- matrix(0, nrow = nDays, ncol = nSeries)
+jumpVariation <- matrix(0, nrow = nDays, ncol = nSeries)
+jumpComponent <- 0
 for(i in 1:nDays){
-  ratio[i,] <- diag(RV[[i]]/BPV[[i]])
+  if(length(rv[[i]]) > 1){ # We have covariance matrix
+    jumpVariation[i,] <- diag((rv[[i]]-bpv[[i]])/rv[[i]])
+  } else { # we only have variance estimates
+    jumpVariation <- pmax((rv - bpv)/rv, 0) # We can just use the XTS's and do it vectorized, truncating at 0
+    jumpComponent <- rv-bpv
+    break 
+  }
 }
 
-colMeans(ratio)^-1
-
-
-plot(sim, observation.based = TRUE)
-
-
+if(nSeries == 1){
+  print(paste("Realized Variance:", mean(rv)))
+  print(paste("Realized Bipower Variation:", mean(bpv)))
+}
 ####### LM jump test #######
-
-pdata <- as.xts.data.table(sample_tdata_microseconds)$PRICE["2018-01-02"]
-alpha <- 0.01
-testing.times <- c("09:30", "09:45", "10:30","10:35","10:40", "10:45", "10:50", "16:00")
-
-LMtest <- intradayJumpTest(pdata, testType = "LM", testing.times = testing.times, window.size = 5, K = 10)
-
 # or equivalently we can specify the jumps in seconds after midnight.
-testing.times <- c(34200, 35100, seq(37800, 39000, 5 * 60), 57600) - 3600
-LMtest <- intradayJumpTest(pdata, testType = "LM", testing.times = testing.times, window.size = 5, K = 10)
+testingTimes <- seq(34200 + 10*300, 57600 - 3600, 300) + 3600
+LMtest <- intradayJumpTest(pData = exp(sim$prices), testType = "LM", testingTimes = testingTimes, windowSize = 5, K = 10)
 
+LMtest1Day <- intradayJumpTest(pData = exp(sim$prices)["1970-01-01"], testType = "LM", testingTimes = testingTimes, windowSize = 5, K = 10)
 
 ### Testing place
-prices <- LMtest[[1]]
-jumps <- na.omit(LMtest[[2]])
-plot(prices)
-eventlines <- rep('Jump', length(jumps))
-eventlines <- xts(eventlines[jumps], index(jumps[jumps]))
-addEventLines(eventlines)
 
-sample.xts <- as.xts(sample_matrix)
-events <- xts(letters[1:3], 
-              as.Date(c("2007-01-12", "2007-04-22", "2007-06-13")))
-plot(sample.xts[,4])
-addEventLines(events, srt=90, pos=2)
-window.size <- 1
-K <- 50
+plot(LMtest1Day)
+plot(LMtest)
+
+
+
+#### Reminder: Seek help with the shading of xts plots when observation.based = TRUE
+timestamps <- rep(0:(10-1), each = 23400) * 86400 + seq(34200, 57600, length.out = 23400)
+x <- xts(cumsum(rnorm(23400 * 10) * sqrt(1/23400)), as.POSIXct(timestamps, origin = "1970-01-01"))
+plot(x)
+
+
+
+
