b0a65c54b79d055a9b0bc8ab46d885a855d261a0
emil <emilsjoerup@live.dk>
2020-07-25 11:06:57 +0200

    using 8 cores for the tests is excessive - using 2 instead.

--- tests/testthat/tests_leadLag.R
+++ tests/testthat/tests_leadLag.R
@@ -12,7 +12,13 @@ test_that("leadLag", {
   0.267945966,0.179464691,0.137441360,0.098206314,0.081605315,0.068544813,0.055210317,0.040167523,0.025934535,0.004943045)
   expect_equal(ll$contrasts, expected)
   ## Test that the parallelized code gives the same results.
-  ll2 <- leadLag(100 + cumsum(sbux), 100 + cumsum(lltc), seq(-10,10), normalize = TRUE, parallelize = TRUE, nCores = 8)
+  ll2 <- leadLag(100 + cumsum(sbux), 100 + cumsum(lltc), seq(-10,10), normalize = TRUE, parallelize = TRUE, nCores = 2)
   expect_true(all.equal(ll,ll2))
+  
+  
+  ## Test that the contrasts are the same when using 10000 ms and 10 sec
+  llMS <- leadLag(100 + cumsum(sbux), 100 + cumsum(lltc), seq(-10,10) * 1000, resolution = "ms", normalize = TRUE)
+  
+  expect_equal(ll$contrasts, llMS$contrasts)
   
 })
