ba3fc33dc88b077c3178a7bc5637e8c30a8dc337
emil <emilsjoerup@live.dk>
2020-09-01 11:51:33 +0200

    Skip on-disk test when not running on a computer with username == "emil"
    Need a better way of doing this though

--- tests/testthat/tests_data_handling.R
+++ tests/testthat/tests_data_handling.R
@@ -110,6 +110,9 @@ test_that("tradesCleanup gives same data as the shipped data", {
 
 test_that("tradesCleanup on-disk functionality", {
   skip_on_cran()
+  if(Sys.getenv("USERNAME") != "emil"){
+    skip("Skipped to not mess with other people's files")
+  }
   library(data.table)
   DT <- SYMBOL <- NULL
   trades2 <- sampleTDataRawMicroseconds
