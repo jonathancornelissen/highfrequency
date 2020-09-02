4ffa0d554a1f5e136297d8c4f3649cbb6281205b
emil <emilsjoerup@live.dk>
2020-09-02 15:32:26 +0200

    References and changes to ReMeDI

--- tests/testthat/tests_realized_measures.R
+++ tests/testthat/tests_realized_measures.R
@@ -326,47 +326,46 @@ test_that("rSemiCov", {
   expect_equal(realized , (mixed + neg + pos))
 })
 
-
 context("ReMeDI")
 
 test_that("ReMeDI", {
   # print("Make sure to implement tests for correctTime = TRUE") ## When it becomes relevant.
   #remed <- ReMeDI(sampleTDataMicroseconds, correctTime = FALSE, lags = 0:25, kn = 2) ##Changed due to correctTime bug
-  remed <- ReMeDI(sampleTDataMicroseconds, lags = 0:25, kn = 2) 
-  
-  expected <- c(1.429670e-05, 9.640725e-05, 1.059059e-04, 7.751148e-05, 3.816783e-05, 1.706490e-05, -9.071417e-06, -4.930293e-06, -2.065554e-06, -2.675700e-05,
-                 -4.750562e-05, -6.652264e-05, -7.900057e-05, -7.084243e-05, -2.944357e-05, -1.196783e-05, 1.206840e-06, 2.660061e-05, 2.610301e-05, 3.354312e-05,
-                 3.074634e-05, -3.352647e-06, -2.297883e-05, -4.176323e-05, -4.162248e-05, -5.299878e-06)
+  remed <- ReMeDI(sampleTDataMicroseconds, lags = 0:25, kn = 2)
+
+  expected <- c(1.431069e-05, 9.651728e-05, 1.060440e-04, 7.762527e-05, 3.823010e-05, 1.709553e-05, -9.089181e-06, -4.940754e-06, -2.070274e-06, 
+                -2.682253e-05, -4.762974e-05, -6.670733e-05, -7.923285e-05, -7.106233e-05, -2.953979e-05, -1.200891e-05, 1.211180e-06,  2.670063e-05, 
+                2.620544e-05, 3.368025e-05, 3.087709e-05, -3.367455e-06, -2.308410e-05, -4.196142e-05, -4.182684e-05, -5.326772e-06)
   
   expect_equal(remed, expected)
-  
+
   # Different data-set
   dat <- sampleTData$PRICE
   storage.mode(dat) <- "numeric"
   #remed <- ReMeDI(dat, correctTime = FALSE, jumpsIndex = NULL, lags = 0:25, kn = 4) ##Changed due to correctTime bug
   remed <- ReMeDI(dat, lags = 0:25, kn = 4)
   
- expected <- c(1.943885e-04, -3.938879e-04, -5.858273e-04, -7.996193e-04, -8.395354e-04, -9.102818e-04, -8.829556e-04, -8.944065e-04, -7.944514e-04, -6.351139e-04,
-               -4.721643e-04, -2.342956e-04, -1.940048e-04, -1.110132e-04, -4.208034e-05, 9.607314e-06,  8.919365e-05,  7.845024e-05, -1.345024e-05, -1.488040e-04,
-               -1.283543e-04, -3.281175e-05, 1.010042e-04, 2.796343e-04,   2.801229e-04,   1.847872e-04)
- 
+   expected <- c(1.946686e-04, -3.945028e-04, -5.868124e-04,  -8.010601e-04, -8.411491e-04, -9.121411e-04, -8.848654e-04, -8.964487e-04,-7.963612e-04,
+                 -6.367172e-04, -4.734131e-04, -2.349435e-04, -1.945647e-04,  -1.113470e-04, -4.221193e-05,  9.638518e-06, 8.949410e-05, 
+                 7.872398e-05,-1.349880e-05, -1.493591e-04, -1.288487e-04, -3.294210e-05,  1.014176e-04, 2.808128e-04,  2.813373e-04,  1.856106e-04)
+
   expect_equal(remed, expected)
-  
-  
+
+
 })
 
 test_that("ReMeDI kn choosing algorithm", {
-  
+
   # optimalKn <- knChooseReMeDI(sampleTDataMicroseconds, correctTime = FALSE, jumpsIndex = NULL, knMax = 10, tol = 0.05, size = 3, lower = 1, upper = 10, plot = FALSE)##Changed due to correctTime bug
   optimalKn <- knChooseReMeDI(sampleTDataMicroseconds, knMax = 10, tol = 0.05, size = 3, lower = 1, upper = 10, plot = FALSE)
   expect_equal(optimalKn, 1L)
-  
+
   dat <- sampleTData$PRICE
   storage.mode(dat) <- "numeric"
   # optimalKn <- knChooseReMeDI(dat, correctTime = FALSE, jumpsIndex = NULL, knMax = 10, tol = 0.05, size = 3, lower = 3, upper = 5, plot = FALSE) ##Changed due to correctTime bug
   optimalKn <- knChooseReMeDI(dat, knMax = 10, tol = 0.05, size = 3, lower = 3, upper = 5, plot = FALSE)
   expect_equal(optimalKn, 4L)
-  
-    
-  
+
+
+
 })
