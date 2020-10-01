82206f0f62d2dfb60621dd32527a0181621c4ffb
Onno Kleen <onnokleen@users.noreply.github.com>
2020-10-01 15:32:16 +0200

    remove quantmod warnings

--- R/getDataFromAlphaVantage.R
+++ R/getDataFromAlphaVantage.R
@@ -34,20 +34,19 @@
 #'                   outputType = "xts", apiKey = 'yourKey')
 #' }
 #' 
-#' @importFrom quantmod getSymbols.av getDefaults
 #' @importFrom data.table as.data.table setnames
 #' @keywords data
 #' @export
 getAlphaVantageData <- function(symbols = NULL, interval = "5min", outputType = "xts", apiKey = NULL, doSleep = TRUE) {
   # Check for API key set in quantmod if it is not present, we kindly remind the user to get it.
-  if (is.null(getDefaults(getSymbols.av, "api.key")$api.key) & is.null(apiKey)) {
+  if (is.null(quantmod::getDefaults(quantmod::getSymbols.av, "api.key")$api.key) & is.null(apiKey)) {
     stop("Your AlphaVantage API key is not set in the quantmod package and no API key was provided. 
           An API key is required but can be obtained for free.
           Please see the getSymbols.av function in the quantmod package for details on how to set a default API key for the current session.
           Alternatively, use the apiKey argument to provide the key.")
   }
   if (is.null(apiKey)) {
-    apiKey <- getDefaults(getSymbols.av, "api.key")$api.key #quantMod handles the extra quotes.
+    apiKey <- quantmod::getDefaults(quantmod::getSymbols.av, "api.key")$api.key #quantMod handles the extra quotes.
   }
   
   
@@ -60,7 +59,7 @@ getAlphaVantageData <- function(symbols = NULL, interval = "5min", outputType = 
     names(data) <- symbols
     for (symbol in symbols) {
       # load the data for the symbol into singleSymbol.
-      singleSymbol <- getSymbols.av(symbol, periodicity = "intraday", interval = interval,
+      singleSymbol <- quantmod::getSymbols.av(symbol, periodicity = "intraday", interval = interval,
                                    api.key = apiKey, auto.assign = FALSE, output.size = "full")
       
       
@@ -82,7 +81,7 @@ getAlphaVantageData <- function(symbols = NULL, interval = "5min", outputType = 
   # We have one symbols
   else {
     # load the data for the symbol into singleSymbol.
-    data <- getSymbols.av(symbols, periodicity = "intraday", interval = interval,
+    data <- quantmod::getSymbols.av(symbols, periodicity = "intraday", interval = interval,
                          api.key = apiKey, auto.assign = FALSE, output.size = "full")  
     if (outputType == "DT") { #Here the user wants a data table out. Thus we set the class DT and we change the names.
       data <- as.data.table(data)
