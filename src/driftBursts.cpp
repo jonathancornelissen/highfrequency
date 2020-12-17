715fa05fedf0ab20f9a69ecc9f0d88eab46f9951
emilsjoerup <emilsjoerup@live.dk>
2020-12-17 13:40:54 +0100

    Do multiplication of inverse instead of division to very slightly increase performance in DBH

--- src/driftBursts.cpp
+++ src/driftBursts.cpp
@@ -14,7 +14,7 @@ const double dC = 2.6614;
 arma::colvec HACWeightC(int iLag){ 
   // Input will always be non-zero and positive, thus, we can ignore the 0 case and save some time
   arma::colvec vW = linspace(1, iLag,iLag) / iLag;
-  int iIdx = floor(iLag/2.0);
+  int iIdx = floor(iLag * 0.5);
   vW(span(0,iIdx-1)) = 1.0 - 6.0 * square(vW(span(0,iIdx-1))) + 6.0 * pow(vW(span(0,iIdx-1)), 3.0);
   vW(span(iIdx, iLag-1)) = 2.0 * pow(1 - vW(span(iIdx, iLag-1)), 3.0);
   return vW;
@@ -48,7 +48,7 @@ double AsymptoticVarianceC(const arma::colvec& vIn, int iLag){
 
 // [[Rcpp::export]]
 int AutomaticLagSelectionC(const arma::colvec& vX, double dMu){
-  int iN = round(4.0 * pow((dMu/100), 0.16));
+  int iN = round(4.0 * pow((dMu * 0.01), 0.16));
   int iT = vX.size();
   if(iT<=iN+1){
     return(iMaxLag);
@@ -80,6 +80,7 @@ int AutomaticLagSelectionC(const arma::colvec& vX, double dMu){
 }
 
 
+
 //[[Rcpp::export]]
 Rcpp::List DriftBurstLoopC(const arma::colvec& vPreAveraged, const arma::colvec& diffedlogprices, 
                            const arma::colvec& vTime, const arma::colvec& vTesttime, 
@@ -96,15 +97,18 @@ Rcpp::List DriftBurstLoopC(const arma::colvec& vPreAveraged, const arma::colvec&
   arma::colvec vWm;
   arma::colvec vWvar;
   
+  const double invMB = 1.0/iMeanBandwidth; // So we can multiply instead of divide which is much faster.
+  const double invVB = 1.0/iVarBandwidth; // So we can multiply instead of divide which is much faster.
+  
   for(int i = 0; i<iT; i++){
     vX = vTime - vTesttime[i];
     iIdx = sum((vX<=0))-2;
     // Construct the weights for the mu estimator using a left-sided exponential kernel
     //  and construct the mu estimate
-    vWm = exp(vX(span(0,iIdx)) / iMeanBandwidth); 
+    vWm = exp(vX(span(0,iIdx)) * invMB); 
     vMu[i] = sum(vWm % vPreAveraged(span(0,iIdx))); 
     // Construct the weights for the sigma estimator using a left-sided exponential kernel
-    vWvar = exp(vX(span(0,iIdx)) / iVarBandwidth);
+    vWvar = exp(vX(span(0,iIdx)) * invVB);
     
     if(iAcLag == -1){
       // Perform automatic lag selection if mandated.
@@ -122,10 +126,10 @@ Rcpp::List DriftBurstLoopC(const arma::colvec& vPreAveraged, const arma::colvec&
     
   }
   // Calculate the test statistic and create a nice named list containing the estimates, which we will return.
-  arma::colvec vDB = sqrt(iMeanBandwidth) * (vMu / iMeanBandwidth) / sqrt(vSigma/ iVarBandwidth) ; 
+  arma::colvec vDB = sqrt(iMeanBandwidth) * (vMu * invMB) / sqrt(vSigma * invVB) ; 
   Rcpp::List lOut = Rcpp::List::create(Rcpp::Named("tStat") = vDB,
-                                       Rcpp::Named("sigma") = vSigma / iVarBandwidth,
-                                       Rcpp::Named("mu") = vMu / iMeanBandwidth);
+                                       Rcpp::Named("sigma") = vSigma * invVB,
+                                       Rcpp::Named("mu") = vMu * invMB);
   return(lOut);
 }
 
@@ -147,9 +151,11 @@ Rcpp::List DriftBurstLoopCPAR(const arma::colvec& vPreAveraged, const arma::colv
   arma::colvec vWm;
   arma::colvec vWvar;
   //Parallelization setup
-#pragma omp parallel for                                                 \
-  shared(vPreAveraged, diffedlogprices, vTime, vTesttime, iMeanBandwidth,\
-         iVarBandwidth, iPreAverage, iAcLag, vMu, vSigma, iT)            \
+  const double invMB = 1.0/iMeanBandwidth; // So we can multiply instead of divide which is much faster.
+  const double invVB = 1.0/iVarBandwidth; // So we can multiply instead of divide which is much faster.
+#pragma omp parallel for                                                               \
+  shared(vPreAveraged, diffedlogprices, vTime, vTesttime, iMeanBandwidth,              \
+         iVarBandwidth, iPreAverage, iAcLag, vMu, vSigma, iT, invMB, invVB)            \
     private(vX, vWm, vWvar, iIdx, iQ, iAutoAcLag)
     for(int i = 0; i<iT; i++){
       
@@ -157,12 +163,12 @@ Rcpp::List DriftBurstLoopCPAR(const arma::colvec& vPreAveraged, const arma::colv
       
       iIdx = sum((vX <= 0)) - 2;
       
-      vWm = exp(vX(span(0, iIdx)) / iMeanBandwidth);  // exponential kernel
+      vWm = exp(vX(span(0, iIdx)) * invMB);  // exponential kernel
       
       
       vMu[i] = sum(vWm(span(0,iIdx)) % vPreAveraged(span(0, iIdx)));
       
-      vWvar = exp(vX(span(0,iIdx)) / iVarBandwidth);  // exponential kernel
+      vWvar = exp(vX(span(0,iIdx)) * invVB);  // exponential kernel
       
       if(iAcLag == -1){
         
@@ -175,10 +181,10 @@ Rcpp::List DriftBurstLoopCPAR(const arma::colvec& vPreAveraged, const arma::colv
       
     }
     
-    arma::colvec vDB = sqrt(iMeanBandwidth) * (vMu/ iMeanBandwidth) / sqrt(vSigma/ iVarBandwidth) ; 
+    arma::colvec vDB = sqrt(iMeanBandwidth) * (vMu * invMB) / sqrt(vSigma * invVB) ; 
   Rcpp::List lOut =  Rcpp::List::create(Rcpp::Named("tStat") = vDB,
-                                        Rcpp::Named("sigma") = vSigma / iVarBandwidth,
-                                        Rcpp::Named("mu") = vMu / iMeanBandwidth);
+                                        Rcpp::Named("sigma") = vSigma * invVB,
+                                        Rcpp::Named("mu") = vMu * invMB);
 #else
   // If openMP is not available, we use single core execution.
   Rf_warning("OpenMP is not available. Sequential processing is used.");
@@ -190,3 +196,4 @@ Rcpp::List DriftBurstLoopCPAR(const arma::colvec& vPreAveraged, const arma::colv
   
   
 }
+
